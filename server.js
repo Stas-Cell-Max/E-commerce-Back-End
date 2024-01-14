c16c7f3b4195d0ecfe4ea45299801679c16f95b4
Stas-Cell-Max <stanislav.morozan@gmail.com>
2024-01-14 13:29:22 -0500

    Integrated Sequlize connection

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection'); // Adjust the path as needed
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Sync sequelize models to the database, then start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
